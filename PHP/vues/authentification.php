475be13c5842e78d6649be95efd08de0a3596f90
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-05 10:32:28 +0200

    graphics changes

--- PHP/vues/authentification.php
+++ PHP/vues/authentification.php
@@ -67,24 +67,5 @@ if(isset($_POST['email']) && isset($_POST['password'])){
     </div>
     
   </body>
-  <footer>
-    <div class="bg-gray-50 mt-6 border-t border-gray-100 px-4 py-2">
-      <dl class="divide-y-4 divide-gray-200">
-        <div class="text-center px-4 py-3">
-          <dt class="text-gray-900 text-lg">Profil Plong&#xE9;e -<span class="text-gray-700 text-lg"> Sans Copyright</span></dt>
-        </div>
-        <div>
-          <div class="text-center px-4 py-2">
-            <dt class="text-gray-900">Valentin DRONNE -<span class="text-gray-700"> valentin@profilplongee.com</span></dt>
-          </div>
-          <div class="text-center px-4 py-2">
-            <dt class="text-gray-900">Robin DELAUNAY -<span class="text-gray-700"> robin@profilplongee.com</span></dt>
-          </div>
-          <div class="text-center px-4 py-2">
-            <dt class="text-gray-900">Ethan HEURTIN -<span class="text-gray-700"> ethan@profilplongee.com</span></dt>
-          </div>
-        </div>
-      </dl>
-    </div>
-  </footer>
+  <?php require("../components/footer.php")?>
 </html>
