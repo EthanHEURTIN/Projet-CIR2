23c37bc79e0213b3971feccd54704657917dd798
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-05 11:27:40 +0200

    changes

--- PHP/constants.php
+++ PHP/constants.php
@@ -1,15 +1,15 @@
 <?php
 
-  const DB_USER = "postgres";
-  const DB_PASSWORD = "Isen44N";
-  const DB_NAME = "profiles_plongee";
-  const DB_SERVER = "127.0.0.1";
-  const DB_PORT = "5432";
+  //const DB_USER = "postgres";
+  //const DB_PASSWORD = "Isen44N";
+  //const DB_NAME = "profiles_plongee";
+  //const DB_SERVER = "127.0.0.1";
+  //const DB_PORT = "5432";
 
   // Database constants.
-  // define('DB_USER', 'isen_grp4');
-  // define('DB_PASSWORD', 'isen2024');
-  // define('DB_NAME', 'profiles_plongee');
-  // define('DB_SERVER', '10.30.51.113');
-  // define('DB_PORT', '5432');
+  define('DB_USER', 'isen_grp4');
+  define('DB_PASSWORD', 'isen2024');
+  define('DB_NAME', 'profiles_plongee');
+  define('DB_SERVER', '10.30.51.113');
+  define('DB_PORT', '5432');
 ?>
