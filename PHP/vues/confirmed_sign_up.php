eef523c3bca9e3a4dd2797c29a2d13b87fa3ad31
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-05 16:42:17 +0200

    u,gu

--- PHP/vues/confirmed_sign_up.php
+++ PHP/vues/confirmed_sign_up.php
@@ -15,7 +15,7 @@ session_start();
           <div class="px-4 py-5 sm:px-6">
             <h3 class="text-lg font-medium leading-6 text-gray-900">Registration confirmed</h3>
             <p class="mt-1 max-w">Your registration has been saved. You can now navigate through the website, create new profiles and view your user profile.</p>
-            <a href="profile.php" class="bg-blue-500 text-white mt-10 rounded-md px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded-md px-3 py-2 text-sm font-medium">Create a new profile</a>  
+            <a href="new_profile.php" class="bg-blue-500 text-white my-5 rounded-md px-3 py-2 text-sm font-medium hover:bg-blue-700 rounded-md px-3 py-2 text-sm font-medium">Go to App</a>
           </div></div></div></div>
   </body>
   <?php require("../components/footer.php")?>
