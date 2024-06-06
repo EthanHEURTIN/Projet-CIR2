ad8ef674465b039b48cff01647f503c26c106b11
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-06 11:31:51 +0200

    zbzf

--- JS/new_profile.js
+++ JS/new_profile.js
@@ -67,7 +67,7 @@ function displayMN90(reponse){
     /* Add button to submit form */
     box = document.getElementById('form');
     box.innerHTML = `
-    <div class="flex items-center justify-center pb-4 py-4">
+    <div class="flex items-center justify-center">
         <button type="submit" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
     </div>
     `
