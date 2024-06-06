7daac93cc0438aa5ce53def0c2d9fecd3bc5b4c9
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-06 16:47:38 +0200

    tgv

--- PHP/vues/new_profile.php
+++ PHP/vues/new_profile.php
@@ -22,9 +22,9 @@ session_start();
     </style>
   <body>
     <?php require_once("../components/header.php");?>
-    <div class="flex flex-col">
+    <div class="flex flex-col justify-between">
       <div class="relative bg-indigo-800 h-dvh w-1/2 mx-auto my-12 shadow-2xl sm:rounded-3xl md:pt-24 lg:flex lg:pt-0 basis-4/5">
-        <div id="box" class="divide-y-3 divide-white mx-auto pb-12 text-center lg:mx-0 lg:flex-auto grid-cols-1">
+        <div id="box" class="divide-y-3 divide-white mx-auto pb-12 text-center lg:mx-0 lg:flex-auto">
           <h3 class="text-4xl font-bold tracking-tight text-white py-5">Generate a new profile</h2>
           <div class="bg-white h-1 w-2/3 mx-auto"></div>
           <form action="profile.php" method="POST">
