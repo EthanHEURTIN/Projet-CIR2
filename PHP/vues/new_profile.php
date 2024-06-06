20f0d1d9f9bd5d9a4f430a1384db150d2b2240da
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-06 09:31:51 +0200

    rfavaerv

--- PHP/vues/new_profile.php
+++ PHP/vues/new_profile.php
@@ -21,19 +21,23 @@ session_start();
   <body>
     <div>
       <div class="mx-auto max-w-7xl py-4 sm:px-6 lg:px-8">
-        <div class="relative isolate overflow-hidden bg-blue-100 px-6 pt-8 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div class="relative isolate overflow-hidden bg-indigo-800 px-6 pt-8 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <div id="box" class="divide-y-4 divide-gray-500 mx-auto text-center lg:mx-0 lg:flex-auto">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl py-8">Generate a new profile</h2>
+            <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl py-8">Generate a new profile</h2>
             <form action="profile.php" method="POST">
-            <div class="grid grid-cols-3 grid-rows-3 gap-4">
-              <p class="mt-6 text-lg text-left leading-8 text-gray-600 grid-cols-1 grid-rows-1">Depth : 
+            <div class="grid grid-cols-3 grid-rows-3 py-5 gap-4">
+              <p class="mt-6 text-lg text-left leading-8 text-white grid-cols-1 grid-rows-1">Depth : 
                 <select name="depth" id="depth" onchange="getMN90ByDepth()" class="px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
-                  <!-- Add depth depending on the depth disponible in db -->
+                </select>
+              </p>
+              <p class="mt-6 text-lg text-left leading-8 text-white grid-cols-1 grid-rows-2">Duration : 
+                <select name="duration" id="duration" class="px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
+                  <!-- Add Time depending on the depth selected -->
                 </select>
               </p>
 
               <!-- Table mn90 show depending on the depth selected -->
-              <div class="scrollable-div grid-cols-2 grid-rows-1 col-span-2 row-span-3  grid-cols-subgrid col-start-2 items-center">
+              <div class="scrollable-div grid-rows-1 col-span-3 row-span-3  grid-cols-subgrid col-start-1 items-center">
                 <div class="px-2 py-2">
                   <table class="m-auto border-double border-4 border-sky-500 text-center">
                       <thead>
@@ -54,12 +58,6 @@ session_start();
                   </table>
                 </div>
               </div>
-
-              <p class="text-lg text-left leading-8 text-gray-600 grid-cols-1">Duration : 
-                <select name="duration" id="duration" class="px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500">
-                  <!-- Add Time depending on the depth selected -->
-                </select>
-              </p>
             </div>
             <div id="form">
               <!-- After Depth select : js Add a form for submit the profil -->
