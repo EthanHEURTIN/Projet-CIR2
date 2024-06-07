5ee814703bda5e2e2afe7db0ec81faf1c3987c82
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-07 16:20:32 +0200

    ac,iacec

--- PHP/vues/sign_up.php
+++ PHP/vues/sign_up.php
@@ -57,7 +57,7 @@
                 <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                   <dt class="text-sm font-medium text-gray-500">Email address</dt>
                   <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">
-                    <input type="text" id="email" name="email" placeholder="Email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                    <input type="email" id="email" name="email" placeholder="Email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                   </dd>
                 </div>
                 <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
