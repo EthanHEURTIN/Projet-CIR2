b0b90ba83882799ed549808935c308c854e438ba
EthanHEURTIN <ethan.heurtin@outlook.com>
2024-06-07 09:03:16 +0200

    Update constants.php

--- PHP/constants.php
+++ PHP/constants.php
@@ -1,15 +1,15 @@
 <?php
 
-  define('DB_USER', 'postgres');
-  define('DB_PASSWORD', 'Isen44N');
-  define('DB_NAME', 'profiles_plongee');
-  define('DB_SERVER', '127.0.0.1');
-  define('DB_PORT', '5432');
+  // define('DB_USER', 'postgres');
+  // define('DB_PASSWORD', 'Isen44N');
+  // define('DB_NAME', 'profiles_plongee');
+  // define('DB_SERVER', '127.0.0.1');
+  // define('DB_PORT', '5432');
 
   // Database constants.
-  // define('DB_USER', 'isen_grp4');
-  // define('DB_PASSWORD', 'isen2024');
-  // define('DB_NAME', 'profiles_plongee');
-  // define('DB_SERVER', '10.30.51.113');
-  // define('DB_PORT', '5432');
+  define('DB_USER', 'isen_grp4');
+  define('DB_PASSWORD', 'isen2024');
+  define('DB_NAME', 'profiles_plongee');
+  define('DB_SERVER', '10.30.51.113');
+  define('DB_PORT', '5432');
 ?>
