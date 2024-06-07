b226a897bd03b2b0dc5f74448bbb3bd6cec40352
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-07 16:31:46 +0200

    eccecq

--- JS/user_profile.js
+++ JS/user_profile.js
@@ -37,7 +37,7 @@ $("#defaultSettingsDiv").on("click", "#buttonSubmitUserSettings", (event) => {
     var capacity = $("#capacityTankInput").val();
     var pressure = $("#pressureTankInput").val();
 
-    ajaxRequest("PUT", ".PHP/vues/request.php/set_user_settings", (response) => {
+    ajaxRequest("PUT", "PHP/vues/request.php/set_user_settings", (response) => {
         if(response){
             $("#msgConfirm").show();
         }
