ff08f951fe9cb306abc53f68973afb6bbf8a3f7a
mkkuu <129859952+mkkuu@users.noreply.github.com>
2024-06-07 15:23:29 +0200

    z dqxsqx

--- PHP/vues/request.php
+++ PHP/vues/request.php
@@ -18,7 +18,6 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 $request = substr($_SERVER['PATH_INFO'], 1);
 $request = explode('/', $request);
 $requestRessource = array_shift($request);
-console.log($requestRessource);
 
 
 if ($requestRessource == 'getMN90Depth'){
